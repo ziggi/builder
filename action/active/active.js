cbbe5ed1a3aa0ed14cecf9ce88061ba99338bd17
ziggi <xziggix@gmail.com>
2014-10-24 23:44:57 +0400

    При нажатии на scrollbar tinymce редактирование не прекращается

--- action/active/active.js
+++ action/active/active.js
@@ -16,11 +16,12 @@ $(function() {
 		var isWidget = $(event.target).parents('.widget').length !== 0;
 		var isWidgetEditor = $(event.target).parents('.widget-editor').length !== 0;
 		//var isTextEditing = $(event.target).parents('.widget-text-editing').length !== 0;
-		var isTextEditing = $(event.target).parents('[id^="mceu_"]').length !== 0;
+		var isTextEditing = $(event.target).parents('[id^="mceu_"]').length !== 0 || $(event.target).find('[id^="mceu_"]').length !== 0;
 		var isAdminClick = $(event.target).parents('.admin-editor').length !== 0;
 
 		if (!isWidget && !isWidgetEditor && !isTextEditing && !isAdminClick) {
 			$('.widget').trigger('inactive');
+			console.log($(event.target).html());
 		}
 	});
 
