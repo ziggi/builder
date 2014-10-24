5e0bd6cbf85fbe6accdde6aa1926c34c5d2a9e07
ziggi <xziggix@gmail.com>
2014-10-24 23:17:47 +0400

    Размер блока страницы ограничивается координатами элементов, включающих высоту

--- widget/block/resize.js
+++ widget/block/resize.js
@@ -1,42 +1,43 @@
 $(function() {
+
+	var BLOCK_MIN_HEIGHT = 10;
+
+	$(document).on('mouseup', function(event) {
+		$(this).unbind('mousemove.page_block');
+	});
 	
 	$(document).on('mousedown', '.page_block .resize-line', function() {
-		$obj = $(this).parent();
+		$block = $(this).parent();
 
-		$(document).bind('mousemove.page_block', function(event) {
-			var new_height = event.pageY - $obj.position().top;
-			var max_height = 0;
+		var new_height = event.pageY - $block.position().top;
+		var max_height = BLOCK_MIN_HEIGHT;
 			
-			$obj.find('.block_inner > div').each(function() {
+		$block.find('.block_inner > div').each(function() {
+			var element_pos = $(this).position().top + $(this).outerHeight();
+
+			if (max_height < element_pos) {
+				max_height = element_pos;
+			}
+		});
+
+		$block.find('.block_inner_big > div').each(function() {
+			if ($(this)[0].style.height !== '100%') {
 				var element_pos = $(this).position().top + $(this).height();
 
 				if (max_height < element_pos) {
 					max_height = element_pos;
 				}
-			});
-
-			$obj.find('.block_inner_big > div').each(function() {
-				if ($(this)[0].style.height === '100%') {
-					max_height = 10;
-				} else {
-					var element_pos = $(this).position().top + $(this).height();
-
-					if (max_height < element_pos) {
-						max_height = element_pos;
-					}
-				}
-			});
-
-			if (new_height > max_height) {
-				$obj.height(new_height);
-			} else {
-				$obj.height(max_height);
 			}
 		});
 
-		$(document).bind('mouseup.page_block', function(event) {
-			$(this).unbind('mousemove.page_block');
-			$(this).unbind('mouseup.page_block');
+		$(document).bind('mousemove.page_block', function(event) {
+			new_height = event.pageY - $block.position().top;
+
+			if (new_height > max_height) {
+				$block.height(new_height);
+			} else {
+				$block.height(max_height);
+			}
 		});
 	});
 
