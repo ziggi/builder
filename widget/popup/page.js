de8c8ef176f7beef10b94b78fd39ef287ef376ef
ziggi <xziggix@gmail.com>
2014-10-28 23:21:49 +0300

    Удалён лишний вызов функции в превью

--- widget/popup/page.js
+++ widget/popup/page.js
@@ -18,7 +18,6 @@ $(function() {
 	$(document).on('click', '.page_blocks_popup .overlay', function() {
 		$('.page_blocks_popup').hide();
 		$('.page_blocks_popup .page_block').hide();
-		$('.page_blocks_popup .page_block').widgetResize('reset');
 	});
 	
 });
