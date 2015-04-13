5af28e2df175865a776c47d430aee0a4eb59eefa
ziggi <xziggix@gmail.com>
2015-04-13 19:41:11 +0300

    #1: active переведён на vanilla js

--- action/active/active.js
+++ action/active/active.js
@@ -1,28 +1,28 @@
-$(function() {
+window.addEventListener('load', function() {
+	[].forEach.call(document.querySelectorAll('.widget-inner'), function(element) {
+		element.addEventListener('mousedown', function() {
+			var widget = this.parentNode;
 
-	$(document).on('mousedown', '.widget-inner', function() {
-		$widget = $(this).parents('.widget');
+			var isTextEditing = widget.querySelectorAll('.widget-text-editing').length !== 0;
+			if (isTextEditing) {
+				return;
+			}
 
-		var isTextEditing = $widget.find('.widget-text-editing').length !== 0;
-		if (isTextEditing) {
-			return;
-		}
-
-		$('.widget.active').trigger('inactive');
-		$widget.trigger('active');
+			$('.widget.active').trigger('inactive');
+			$(widget).trigger('active');
+		});
 	});
 
-	$(document).on('mousedown', function(event) {
-		var isWidget = $(event.target).parents('.widget').length !== 0;
-		var isWidgetEditor = $(event.target).parents('.widget-editor').length !== 0;
-		//var isTextEditing = $(event.target).parents('.widget-text-editing').length !== 0;
-		var isTextEditing = $(event.target).parents('[id^="mceu_"]').length !== 0 || $(event.target).find('[id^="mceu_"]').length !== 0;
-		var isAdminClick = $(event.target).parents('.admin-editor').length !== 0;
-		var isPalette = $(event.target).parents('.sp-container').length !== 0;
+	document.addEventListener('mousedown', function(event) {
+		var isWidget = event.target.parents('.widget').length !== 0;
+		var isWidgetEditor = event.target.parents('.widget-editor').length !== 0;
+		//var isTextEditing = event.target.parents('.widget-text-editing').length !== 0;
+		var isTextEditing = event.target.parents('[id^="mceu_"]').length !== 0 || event.target.querySelectorAll('[id^="mceu_"]').length !== 0;
+		var isAdminClick = event.target.parents('.admin-editor').length !== 0;
+		var isPalette = event.target.parents('.sp-container').length !== 0;
 
 		if (!isWidget && !isWidgetEditor && !isTextEditing && !isAdminClick && !isPalette) {
 			$('.widget.active').trigger('inactive');
 		}
 	});
-
 });
