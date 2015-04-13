c348e0d094c239b935ea3f404cbb229048493915
ziggi <xziggix@gmail.com>
2015-04-13 18:27:05 +0300

    #1: hover перенесён на forEach

--- action/hover/hover.js
+++ action/hover/hover.js
@@ -1,16 +1,14 @@
 window.addEventListener('load', function() {
-	var elements = document.querySelectorAll('.widget');
-
-	for (var i = 0; i < elements.length; i++) {
-		elements[i].addEventListener('mouseenter', function() {
+	[].forEach.call(document.querySelectorAll('.widget'), function(element) {
+		element.addEventListener('mouseenter', function() {
 			var isActive = this.querySelectorAll('.widget.active').length !== 0;
 			if (!isActive) {
 				this.classList.add('hover');
 			}
 		});
 
-		elements[i].addEventListener('mouseleave', function() {
+		element.addEventListener('mouseleave', function() {
 			this.classList.remove('hover');
 		});
-	}
+	});
 });
