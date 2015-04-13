f4f937bab20f4dec9f0624f6b4d97f21911b23a2
ziggi <xziggix@gmail.com>
2015-04-13 17:31:39 +0300

    #1: Hover migration to native js

--- action/hover/hover.js
+++ action/hover/hover.js
@@ -1,14 +1,16 @@
-$(function() {
+window.addEventListener('load', function() {
+	var elements = document.querySelectorAll('.widget');
 
-	$(document).on('mouseenter', '.widget', function() {
-		var isActive = $('.widget.active').length !== 0;
-		if (!isActive) {
-			$(this).addClass('hover');
-		}
-	});
-	
-	$(document).on('mouseleave', '.widget', function() {
-		$(this).removeClass('hover');
-	}); 
+	for (var i = 0; i < elements.length; i++) {
+		elements[i].addEventListener('mouseenter', function() {
+			var isActive = this.querySelectorAll('.widget.active').length !== 0;
+			if (!isActive) {
+				this.classList.add('hover');
+			}
+		});
 
+		elements[i].addEventListener('mouseleave', function() {
+			this.classList.remove('hover');
+		});
+	}
 });
